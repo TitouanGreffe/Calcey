a072eadd745435033757aca919eb0be723d2c9eb
eymontem <erica.montemayor@lca-net.com>
2024-05-24 06:32:04 +0200

    updated

--- calcey/test_pest_model.py
+++ calcey/test_pest_model.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import bw2data as bd
 
 pesticide_quantity_dict = {
     'pesticide': ['DDT', 'Copper', '2,4-DB'],
     'quantity': [0.206, 0.250, 0.250]
 }
-df_pesticide_quantity =pd.DataFrame(pesticide_quantity_dict)
+df_pesticide_quantity = pd.DataFrame(pesticide_quantity_dict)
 
 # Emission factors
 EF_pest_soil = 0.9
@@ -16,19 +15,22 @@ results = []
 
 # Iterate through the DataFrame rows
 for index, row in df_pesticide_quantity.iterrows():
-     pest_name = row['pesticide_name']
-     pest_quantity = row['value']
+    pesticide_name = row['pesticide']
+    pest_quantity = row['quantity']
     
-     # Calculate emissions
-     pest_em_soil = pest_quantity * EF_pest_soil
-     pest_em_air = pest_quantity * EF_pest_air
-       
-     # Append the results as a dictionary
-     results.append({
-         'pesticide_name': pest_name,
-         'pest_em_soil': pest_em_soil,
-         'pest_em_air': pest_em_air
-        })
+    # Calculate emissions
+    pest_em_soil = pest_quantity * EF_pest_soil
+    pest_em_air = pest_quantity * EF_pest_air
+    
+    # Append the results as a dictionary
+    results.append({
+        'pesticide_name': pesticide_name,
+        'pest_em_soil': pest_em_soil,
+        'pest_em_air': pest_em_air
+    })
 
 # Convert the results list to a DataFrame
 df_emissions = pd.DataFrame(results)
+
+# Display the results DataFrame
+df_emissions
