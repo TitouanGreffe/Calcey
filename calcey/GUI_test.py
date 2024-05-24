a66abd93883bb8bcbaa114bf75659eaccd1e405f
gavinmavin <100697886+gavinmavin@users.noreply.github.com>
2024-05-24 15:04:21 -0400

    amazing work everybody

--- calcey/GUI_test.py
+++ calcey/GUI_test.py
@@ -17,60 +17,177 @@ from scipy import sparse
 import uuid
 from geopy.geocoders import Nominatim
 
-class calcey:
-    def __init__(self):
+import customtkinter
+from tkintermapview import TkinterMapView
+import panel as pn
+
+BIOSPHERE_FLOW_DATABASE_LABELS = ["ecoinvent-3.9.1-biosphere"]
+
+class Purolator(bi.package.BW2Package):
+    @classmethod
+    def _create_obj(cls, data):
+        instance = data["class"](data["name"])
+
+        if data["name"] not in instance._metadata:
+            instance.register(**data["metadata"])
+        else:
+            instance.backup()
+            instance.metadata = data["metadata"]
+    
+        flows = {
+            (obj['database'], obj['code']) 
+            for label in BIOSPHERE_FLOW_DATABASE_LABELS
+            for obj in bd.Database(label)
+        }
+        
+        if isinstance(instance, bd.Method):
+            data['data'] = [line for line in data['data'] if line[0] in flows]
+        
+        instance.write(data["data"])
+        return instance
+
+
+class calcey(customtkinter.CTk):
+
+    APP_NAME = "Calcey"
+    WIDTH = 1125
+    HEIGHT = 590
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.df_crops = pd.read_excel('../data/Mapping_data_Calcey.xlsx', sheet_name='Mapping_Fertilizer', usecols='A')
+
+        self.title(calcey.APP_NAME)
+        self.geometry(str(calcey.WIDTH) + "x" + str(calcey.HEIGHT))
+        self.minsize(calcey.WIDTH, calcey.HEIGHT)
+
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.bind("<Command-q>", self.on_closing)
+        self.bind("<Command-w>", self.on_closing)
+        self.createcommand('tk::mac::Quit', self.on_closing)
+        
         bd.projects.set_current("calcey")
             
         self.brightway_checkin()
+        #self.import_method()
+        # ============ create two CTkFrames ============
+
+        self.frame_left = customtkinter.CTkFrame(master=self, corner_radius=0, fg_color=None)
+        self.frame_left.grid(row=0, column=0, padx=1, pady=0, sticky="nsew")
+
+        self.frame_right = customtkinter.CTkFrame(master=self, corner_radius=0, width=100)
+        self.frame_right.grid(row=0, column=1, rowspan=1, pady=0, padx=0, sticky="nsew")
+
+        # ============ frame_left ============
+
+        customtkinter.set_default_color_theme("blue")
+        self.result_label = customtkinter.CTkLabel(self.frame_left, text="Result:", font=customtkinter.CTkFont(family="Circular Std Black", size=15))
+        self.result_label.grid(row=6, column=0, padx=(10, 10), pady=(20, 0))
+
+        self.result_entry = customtkinter.CTkEntry(self.frame_left, width=150)
+        self.result_entry.grid(row=7, column=0, padx=(10, 10), pady=(5, 0))
+
+        self.label = customtkinter.CTkLabel(self.frame_left, anchor=customtkinter.N,text="Welcome to Calcey!",  width=600, height=50, fg_color='transparent',font=customtkinter.CTkFont(family="Circular Std Black",size=30, weight="bold"))
+        self.label.grid(row=0, column=0, padx=(10, 10), pady=(5, 20), columnspan=2)
+
+        self.label2 = customtkinter.CTkLabel(self.frame_left, anchor=customtkinter.W, text="1. Please click on the map to get your coordinates.\n 2. Select the right crop.\n 3. Optional: Enter the amount for yield and fertilizer.\n 4. Press submit.",  width=500, height=50, fg_color='transparent',font=customtkinter.CTkFont(family="Circular Std Black",size=20, weight="bold"))
+        self.label2.grid(row=1, column=0, padx=(10, 10), pady=(5, 30), columnspan=2)
+    
+
+        self.longitude_label = customtkinter.CTkLabel(self.frame_left, text="Longitude:", font=customtkinter.CTkFont(family="Circular Std Black", size=15))
+        self.longitude_label.grid(row=2, column=0, padx=(10, 10), pady=(20, 0))
+
+        self.latitude_label = customtkinter.CTkLabel(self.frame_left, text="Latitude:", font=customtkinter.CTkFont(family="Circular Std Black", size=15))
+        self.latitude_label.grid(row=4, column=0, padx=(10, 10), pady=(20, 0))
+
+        self.longitude_entry = customtkinter.CTkEntry(self.frame_left, width=150)
+        self.longitude_entry.grid(row=3, column=0, padx=(10, 10), pady=(5, 0))
+
+        self.latitude_entry = customtkinter.CTkEntry(self.frame_left, width=150)
+        self.latitude_entry.grid(row=5, column=0, padx=(10, 10), pady=(5, 20))
+
+        self.crop_label = customtkinter.CTkLabel(self.frame_left, text="Please select your crop", font=customtkinter.CTkFont(family="Circular Std Black", size=15))
+        self.crop_label.grid(row=2, column=1, padx=(10, 10), pady=(20, 0))
         
-        self.root = tk.Tk()
-        self.frame = tk.Frame(self.root)
-        self.frame.pack()
+        self.crop_entry = customtkinter.CTkOptionMenu(self.frame_left, values = list(self.df_crops['List_UI']),width=300 )
+        self.crop_entry.grid(row=3, column=1, padx=(10, 10), pady=(5, 0))
 
-        self.input_field_lat = tk.Entry(self.frame, width=20)
-        self.input_field_lat.pack()
+        self.yield_label = customtkinter.CTkLabel(self.frame_left, text="Please enter your yield in kg/ha:", font=customtkinter.CTkFont(family="Circular Std Black", size=15))
+        self.yield_label.grid(row=4, column=1, padx=(10, 10), pady=(20, 0))
+        
+        self.yield_entry = customtkinter.CTkEntry(self.frame_left,width=300 )
+        self.yield_entry.grid(row=5, column=1, padx=(10, 10), pady=(5, 20))
 
-        self.input_field_lon = tk.Entry(self.frame, width=20)
-        self.input_field_lon.pack()
+        self.fertilizer_label = customtkinter.CTkLabel(self.frame_left, text="Please enter your fertilizer amount in kgN/ha:", font=customtkinter.CTkFont(family="Circular Std Black", size=15))
+        self.fertilizer_label.grid(row=6, column=1, padx=(10, 10), pady=(20, 0))
+        
+        self.fertilizer_entry = customtkinter.CTkEntry(self.frame_left,width=300 )
+        self.fertilizer_entry.grid(row=7, column=1, padx=(10, 10), pady=(5, 10))
 
-        self.input_field_crop = tk.Entry(self.frame, width=20)
-        self.input_field_crop.pack()
+        self.submit_button = customtkinter.CTkButton(master = self, height=50, width=1110, text="Submit", font=customtkinter.CTkFont(family="Circular Std Black", size=20, weight="bold"), command=self.print_result)
+        self.submit_button.grid(column=0, row=13, columnspan=5, pady=(20, 0), padx=(0, 0))
 
-        self.button = tk.Button(self.frame, 
-                               text="CLICK ME", 
-                               fg="red",
-                               command=self.print_result)
+        # ============ frame_right ============
 
-        self.button.pack(side=tk.LEFT)
+        self.frame_right.grid_rowconfigure(0, weight=0)
+        self.frame_right.grid_columnconfigure(0, weight=1)
+        
 
+        self.map_widget = TkinterMapView(self.frame_right, corner_radius=20, width=500, height=500)
+        self.map_widget.add_left_click_map_command(self.get_coordinates)
+        self.map_widget.grid(row=0, rowspan=1, column=0, columnspan=3, sticky="nswe", padx=(0, 0), pady=(0, 0))
+
+
+    def get_coordinates(self, coordinates_tuple):
+        longitude = coordinates_tuple[1]
+        latitude = coordinates_tuple[0]
+        self.longitude_entry.delete(0,"end")
+        self.latitude_entry.delete(0,"end")
+        self.longitude_entry.insert(0, longitude)
+        self.latitude_entry.insert(0, latitude)
+        marker = self.map_widget.set_marker(latitude, longitude)
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+    def start(self):
+        self.mainloop()
 
     def print_result(self):
         
         # get all inputs from the UI
-        value_lat = float(self.input_field_lat.get())
-        value_lon = float(self.input_field_lon.get())
-        crop = self.input_field_crop.get()
+        value_lon = float(self.longitude_entry.get())
+        value_lat = float(self.latitude_entry.get())
+        crop = self.crop_entry.get()
+        input_yield = self.yield_entry.get()
+        n_fert_input = self.fertilizer_entry.get()
+
+        if input_yield == "":
+            input_yield = None
+        else:
+            input_yield = float(input_yield)
+
+        if n_fert_input == "":
+            n_fert_input = None
+        else:
+            n_fert_input = float(n_fert_input)
+        
+        #value_lat = float(self.input_field_lat.get())
+        #value_lon = float(self.input_field_lon.get())
+        #crop = self.input_field_crop.get()
         # input_yield = self.input_field_yield.get()   n_fert_input
         
         self.create_main_process(self.name_database, value_lat, value_lon, crop)
         # yield_ = input_yield       n_fert_input
         print("create_main_process finished")
-        self.dataframe_exchanges(latitude=value_lat, longitude=value_lon, crop=crop)
+        self.dataframe_exchanges(latitude=value_lat, longitude=value_lon, crop=crop, n_fert_input=n_fert_input)
         print("dataframe_exchanges finished")
         self.generate_edges_to_product()
         print("generate_edges_to_product finished")
         self.calculate_LCA_results()
-
-    def get_background_data_yield(self, input_lat: float, input_lon: float, crop: str):
-        # get yield data
-        df = pd.read_csv("../data/FAOSTAT_data_yield.csv")
-        country = self.country_from_coordinates(Latitude=input_lat, Longitude=input_lon)
-        FAO_yield = float(df.loc[(df['Area'] == country) & (df['Year'] == 2022) & (df['Item'] == crop), 'Value'].iloc[0])
-        return FAO_yield
+        #self.calculate_multi_LCA_results()
     
-    def run(self):
-        self.root.mainloop()
-
     def country_from_coordinates(self, Latitude: float, Longitude: float):
         lat_str = str(Latitude)
         lon_str = str(Longitude)
@@ -328,6 +445,30 @@ class calcey:
         all_inputs = list(set(self.mapping_flows_uuid.index))
         all_inputs = [x for x in all_inputs if type(x) != float]
         self.df_output = pd.DataFrame(index = all_inputs , columns = ['Amount'])
+        if yield_ is None and n_fert_input is not None:
+            print("There is yield input and nitrogen fertilizer input, thanks!")
+            yield_data = self.get_background_data_yield(latitude,longitude,crop)
+            pest_data, pest_total = self.get_background_data_pest(latitude,longitude,crop) ## add self.
+            fert_data = self.get_background_data_fert(latitude,longitude,crop) ## add self.
+            water_data = self.get_background_data_water(latitude,longitude,crop) ## add self.
+            generic_yield, df_fert_per_kg, water_m3_per_kg, df_pest_per_kg, total_pest_per_kg = self.create_final_dataframe(yield_data,fert_data,water_data,pest_data,pest_total) ## add self.
+            self.df_output.loc["Default_pesticide","Amount"] = total_pest_per_kg
+            self.df_output.loc["Default_irrigation","Amount"] = water_m3_per_kg/0.8
+            self.df_output.loc["Default_nitrogen_fertilizer","Amount"] = n_fert_input
+            self.df_output.loc["Default_p2o5_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_P2O5_applied","Value"]
+            self.df_output.loc["Default_k2o_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_K2O_applied","Value"]
+            self.df_output.loc["Dinitrogen monoxide_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_N2O","Value"]
+            self.df_output.loc["Ammonia_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_NH3","Value"]
+            self.df_output.loc["Nitrate_to_water","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_NO3","Value"]
+            self.df_output.loc["Nitrogen_oxides_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_NOx","Value"]
+            self.df_output.loc["Phosphate_to_water","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_P","Value"]
+            list_pesticides = list(pest_data.index)
+            list_compartments = ["pest_em_soil", "pest_em_air"]
+            for i in list_pesticides:
+                for compartment in list_compartments:
+                    df_pesticides_emissions = self.calculate_pest_emissions(i,pest_data.loc[i,"value"])
+                    self.df_output.loc[str(i)+""+compartment,"Amount"] = df_pesticides_emissions.loc[i,compartment]
+            self.df_output = self.df_output.fillna(0)
         if yield_ is not None and n_fert_input is not None:
             print("There is yield input and nitrogen fertilizer input, thanks!")
             pest_data, pest_total = self.get_background_data_pest(latitude,longitude,crop) ## add self.
@@ -339,6 +480,11 @@ class calcey:
             self.df_output.loc["Default_nitrogen_fertilizer","Amount"] = n_fert_input
             self.df_output.loc["Default_p2o5_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_P2O5_applied","Value"]
             self.df_output.loc["Default_k2o_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_K2O_applied","Value"]
+            self.df_output.loc["Dinitrogen monoxide_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_N2O","Value"]
+            self.df_output.loc["Ammonia_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_NH3","Value"]
+            self.df_output.loc["Nitrate_to_water","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_NO3","Value"]
+            self.df_output.loc["Nitrogen_oxides_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_NOx","Value"]
+            self.df_output.loc["Phosphate_to_water","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_P","Value"]
             list_pesticides = list(pest_data.index)
             list_compartments = ["pest_em_soil", "pest_em_air"]
             for i in list_pesticides:
@@ -358,6 +504,10 @@ class calcey:
             self.df_output.loc["Default_nitrogen_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_N_applied","Value"]
             self.df_output.loc["Default_p2o5_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_P2O5_applied","Value"]
             self.df_output.loc["Default_k2o_fertilizer","Amount"] = df_fert_per_kg.loc["Mean_K2O_applied","Value"]
+            self.df_output.loc["Ammonia_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_NH3","Value"]
+            self.df_output.loc["Nitrate_to_water","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_NO3","Value"]
+            self.df_output.loc["Nitrogen_oxides_to_air","Amount"] = df_fert_per_kg.loc["Mean_Emission_NOx","Value"]
+            self.df_output.loc["Phosphate_to_water","Amount"] = df_fert_per_kg.loc["Mean_Emission_kg_P","Value"]
             list_pesticides = list(pest_data.index)
             list_compartments = ["pest_em_soil", "pest_em_air"]
             for i in list_pesticides:
@@ -415,6 +565,19 @@ class calcey:
         self.db = bd.Database(self.name_database)
         self.db.register()
 
+    def import_method(self):
+        Purolator.import_file("impact_world_plus_201_brightway2_expert_version.e2a8415edfe998674fba2e09bddfd831.bw2package")
+
+    def calculate_multi_LCA_results(self):
+        impact_categories = [i for i in bd.methods if "World+" in i[0] and 'Total' in i[2]]
+        print(impact_categories)
+        #name_database = "ecoinvent 3.9.1 cutoff"
+        bd.calculation_setups['bw2_training comparison'] = {'inv':[{self.product:1},
+                                                            ],
+                                                     'ia':impact_categories}
+        multilca = bc.MultiLCA('bw2_training comparison')
+        self.results = pd.DataFrame(multilca.results, columns=[i[2] for i in impact_categories])
+        self.results.plot(kind='bar')
 
     def calculate_LCA_results(self):
         lca = bc.LCA(
@@ -425,4 +588,5 @@ class calcey:
         )
         lca.lci()
         lca.lcia()
+        self.result_entry.insert(0, str(lca.score))
         print(lca.score)
